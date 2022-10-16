6f03976019cbe8c6543b65e84c8a26cadc68f2ca
Denzel-Britton <denzel.britton573@gmail.com>
2022-10-16 01:21:34 -0500

    fixed shadow box issue refactor the serch function in js.file

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -104,7 +104,7 @@ function generatePostsHTML(posts) {
                  
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<div id="container">
+<div class="container" id="container">
 <div class="row">
 <div class="column">
 <div class="product-details">
