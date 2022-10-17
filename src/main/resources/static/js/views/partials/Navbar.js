75c908ab2e27ff8e5221c9890bc245b48fd13946
Benjamin Marks <bjmarks13@yahoo.com>
2022-10-17 14:08:29 -0500

    Update Navbar.js

--- src/main/resources/static/js/views/partials/Navbar.js
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -2,6 +2,7 @@ import {isLoggedIn} from "../../auth.js";
 
 export default function Navbar(props) {
     const loggedIn = isLoggedIn();
+     const userId = JSON.parse(localStorage.getItem('user'));
     let html = `
         <nav>
             <a class="jalopy-nav" href="/" data-link>Home</a>`;
@@ -9,7 +10,7 @@ export default function Navbar(props) {
     if(loggedIn) {
         html += `
             <a class="jalopy-nav" href="/posts" data-link>Posts</a>
-            <a class="jalopy-nav" href="/user" id="user" data-link>User</a>
+            <a class="jalopy-nav" href="/user" id="user" data-user-id="${userId.id}" data-link>User</a>
             <a class="jalopy-nav" href="/logout" data-link>Logout</a>`;
     } else {
         html += `<a class="jalopy-nav" href="/login" data-link>Login</a>`;
