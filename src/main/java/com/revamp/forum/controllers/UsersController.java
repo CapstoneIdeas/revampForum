992a9612288f74b0439a94b5df5b4d2a7544f930
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-10-17 16:35:04 -0500

    updated path on user controller

--- src/main/java/com/revamp/forum/controllers/UsersController.java
+++ src/main/java/com/revamp/forum/controllers/UsersController.java
@@ -20,7 +20,7 @@ import java.util.Optional;
     @CrossOrigin
     @AllArgsConstructor
     @RestController
-    @RequestMapping(value = "/api/users", produces = "application/json")
+    @RequestMapping(value = "/api/user", produces = "application/json")
     public class UsersController {
         private UsersRepository usersRepository;
         private AuthBuddy authBuddy;
